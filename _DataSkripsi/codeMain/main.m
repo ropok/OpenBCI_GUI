6d4632ef3245f77edc2f73217408d8f832fdcde5
ropok <jalerse@gmail.com>
2018-04-02 13:31:28 +0700

    Plot PSD untuk subjek2c

--- _DataSkripsi/codeMain/main.m
+++ _DataSkripsi/codeMain/main.m
@@ -2,20 +2,23 @@
 
 close all; clear; clc;
 
+subjek = 'subjek2c_22.txt';
 folder1 = 'D:\Jaler\OpenBCI_GUI\_DataSkripsi\Dataset\rawData\';
+folder2 = 'D:\Jaler\OpenBCI_GUI\_DataSkripsi\Dataset\rawData\temp12\';
 files = dir([folder1 '*.txt']);
 fs = 200;
 % - loadRaw - Fnotch50Hz - BP1-50Hz
-data = load([folder1 'subjek2c_24.txt']);
+data = load([folder1 subjek]);
 data = filterNotch50(data, fs);
 [data,BP] = filterBP(data, fs, 1, 50);
-% [data,BP] = filterBP(data, fs, 14, 26);
+[data,BP] = filterBP(data, fs, 8, 26);
 [M,B,durasi] = potongData(data, fs, 2);
 M_PSD = PSDAll(M, fs);
 B_PSD = PSDAll(B, fs);
 [xlim1, xlim2, durasi, BPLabel] = labelPlot(5, 30, durasi, BP);
-plotPSD1(M_PSD, 'Merah', xlim1, xlim2, durasi, BPLabel);
-plotPSD1(B_PSD, 'Biru', xlim1, xlim2, durasi, BPLabel);
+subjek(regexp(subjek, '[.txt]'))=[];
+plotPSD1(M_PSD, 'Merah-9Hz', xlim1, xlim2, durasi, BPLabel); print([folder2 sprintf('M_PSD_%s',subjek)],'-dpng');
+plotPSD1(B_PSD, 'Biru-14Hz', xlim1, xlim2, durasi, BPLabel); print([folder2 sprintf('B_PSD_%s',subjek)],'-dpng');
 
 % data11 = filterBP(data, fs, 10.5, 11.5);
 % data13 = filterBP(data, fs, 12.5, 13.5);
--- _DataSkripsi/codeMain/plotPSD1.m
+++ _DataSkripsi/codeMain/plotPSD1.m
@@ -21,21 +21,21 @@ function plotPSD1(PSDdata, fokusWarna, xlim1, xlim2, durasi, BPLabel)
     figure();
         for i = 1:sizeData(1) % Time Frame
             subplot(3,1,i)
-            if i == 1
-                title({sprintf('%s (BP: %d-%d Hz)',fokusWarna, BPLabel); sprintf('%d-%d s', detik(i),detik(i)+durasi)});
-                else    
-                    title( sprintf('%d-%d s', detik(i),detik(i)+durasi) );
-                end
                 hold on;
                 for j = 1:sizeData(2) % Channel
                     plot(PSDdata(i,j).frequencies, PSDdata(i,j).data);
                 end
                 hold off;
+            if i == 1
+                title({sprintf('%s (BP: %d-%d Hz)',fokusWarna, BPLabel); sprintf('%d-%d s', detik(i),detik(i)+durasi)});
+                        legend('Fp_1','Fp_2','C_3', 'C_4' , 'Location','northeast' , 'Orientation','horizontal');
+
+                else    
+                    title( sprintf('%d-%d s', detik(i),detik(i)+durasi) );
+                end
             xlim([xlim1 xlim2]);
-            ylim([minData maxData]);
+            % ylim([minData maxData]);
             xlabel('\fontsize{8}Frequency (Hz)');
             ylabel('\fontsize{8}Data');
         end
-        legend('Fp_1','Fp_2','C_3', 'C_4' , 'Location','best' , 'Orientation','horizontal');
-
 end
