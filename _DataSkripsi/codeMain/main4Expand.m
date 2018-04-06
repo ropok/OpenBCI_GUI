aad906f8eb3f7d45429d301bf8c31d00664089bd
ropok <jalerse@gmail.com>
2018-04-06 09:47:21 +0700

    temp14 : Confusion Matrix dan Performance

--- _DataSkripsi/codeMain/main4Expand.m
+++ _DataSkripsi/codeMain/main4Expand.m
@@ -20,27 +20,8 @@ end
 
 % -- JST
 inputs = [data_rM' data_rB']; % input untuk JST
-[~,N] = size(inputs);
-n = N/2;
-targets(1,1:n) = ones;
-    targets(2,n+1:N) = ones;
+% JST(inputs, 10);
 
-% Membuat Pattern Recognition Network
-hiddenLayerSize = 10;
-net = patternnet(hiddenLayerSize);
-
-% Set up Division of Data for Training, Validation, Testing
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-
-% Train the Network
-[net,tr] = train(net, inputs, targets);
-
-% Test the Network
-outputs = net(inputs);
-errors = gsubtract(targets, outputs);
-performance = perform(net, targets, outputs)
-
-% View the Network
-view(net)
+for i = 1:9
+    inputs1{1,i} = inputs(4*i-3:4*i,:);
+end
