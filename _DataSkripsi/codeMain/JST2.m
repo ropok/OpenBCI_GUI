6e54f14df81988d9e5c7b978b241db0d8e56bd2d
ropok <jalerse@gmail.com>
2018-04-09 15:16:59 +0700

    Update Confusion matrix 1

---
+++ _DataSkripsi/codeMain/JST2.m
@@ -0,0 +1,33 @@
+function [targets, outputs] = JST2(inputs, hiddenLayerSize)
+
+    % Generate targets
+    [~,N] = size(inputs);
+    n = N/2;
+    targets(1,1:n) = ones;
+        targets(2,n+1:N) = ones;
+
+    % Membuat Pattern Recognition Network
+    % hiddenLayerSize = 10;
+    net = patternnet(hiddenLayerSize);
+
+    % Set up Division of Data for Training, Validation, Testing
+    net.divideParam.trainRatio = 70/100;
+    net.divideParam.valRatio = 15/100;
+    net.divideParam.testRatio = 15/100;
+
+    % Train the Network
+    [net,tr] = train(net, inputs, targets);
+    nntraintool('close'); % close the nntraintool immediately
+
+    % Test the Network
+    outputs = net(inputs);
+    errors = gsubtract(targets, outputs);
+    performance = perform(net, targets, outputs);
+
+    % % Saving the confusionMatrix plot
+    % figure, plotconfusion(targets, outputs);
+    % print([folder2 sprintf('M_PSD_%s',subjek)],'-dpng');
+
+    % View the Network
+    % view(net);
+end
--- _DataSkripsi/codeMain/main4Expand.m
+++ _DataSkripsi/codeMain/main4Expand.m
@@ -1,16 +1,17 @@
 close all; clear; clc;
 
 folder1 = 'D:\Jaler\OpenBCI_GUI\_DataSkripsi\Dataset\rawData\';
-folder2 = 'D:\Jaler\OpenBCI_GUI\_DataSkripsi\Dataset\rawData\temp14\';
+folder2 = 'D:\Jaler\OpenBCI_GUI\_DataSkripsi\Dataset\rawData\temp17\';
 
 data_rM = [];
 data_rB = [];
 
 files = dir([folder1 '*.txt']);
 
+% Mengumpulkan data dari semua objek
 for h = 1:length(files)
 % for h = 1:2 % for Test purpose
-    [rM, rB] = main2(files(h,1).name, 1);
+    [rM, rB] = main4(files(h,1).name, 2);
 
     % -- Mengumpulkan rM seluruh subjek ------
     data_rM = [data_rM; rM];
@@ -20,11 +21,44 @@ for h = 1:length(files)
     loading('Load Data', h, length(files));
 end
 
-% -- JST
-inputs = [data_rM' data_rB']; % input untuk JST
-% JST(inputs, 10);
+% % -- JST --
+% inputs = [data_rM' data_rB']; % input untuk JST
+% for i = 1:9
+%     inputs1{1,i} = inputs(4*i-3:4*i,:);
+%     % Saving the confusionMatrix plot
+%     [targets, outputs] = JST2(inputs1{1,i},10);
+%     figure, plotconfusion(targets, outputs, sprintf('%d',i));
+%     print([folder2 sprintf('Confusion_Matrix_%d',i)],'-dpng');
+%     close all;
+%     clear targets outputs;
+%     loading('Confusion Matrix Plotting', i, 9);
+% end
 
-for i = 1:9
-    inputs1{1,i} = inputs(4*i-3:4*i,:);
-    % loading('inputs', i, 9);
+% % -- Variasi i
+%     iMin = 1;
+%     iMax = 10;
+% -- Variasi j (Hidden Layer)
+    jMin = 1;
+    jMax = 20;
+
+% -- JST -- Variasi Hidden Layer 1-10
+inputs = [data_rM' data_rB']; % input untuk JST
+for i = 1:10
+    if i == 10
+        inputs1{1,i} = inputs;
+    else
+        inputs1{1,i} = inputs(4*i-3:4*i,:);
+    end
+    % Saving the confusionMatrix plot
+    textKelompok = sprintf('KelompokCiri%d',i);
+    for j = jMin:jMax
+        textHiddenLayer = sprintf('HiddenLayer%d',j);
+        [targets, outputs] = JST2(inputs1{1,i},j);
+        figure, plotconfusion(targets, outputs, [textKelompok ' ' textHiddenLayer ' ']);
+        print([folder2 sprintf('CM_%s_%s',textKelompok,textHiddenLayer)],'-dpng');
+        close all;
+        clear targets outputs;
+        loading(sprintf('Plotting Kelompok %d',j), j, jMax);
+    end
+    loading('Confusion Matrix Plotting', i, 10);
 end
