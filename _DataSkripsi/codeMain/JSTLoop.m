c8422610e41c8ee08b7c92fd46940025f280a5a5
ropok <jalerse@gmail.com>
2018-05-22 12:18:19 +0700

    dataRaw2/temp2/JST_23.m net dan akurasi TPR FDR nya

--- _DataSkripsi/codeMain/JSTLoop.m
+++ _DataSkripsi/codeMain/JSTLoop.m
@@ -2,14 +2,13 @@
 close all; clc;
 labelDataLatih = {'tabel32Ciri_Sesi1' 'tabel32Ciri_Sesi2' 'tabel32Ciri_Sesi1Sesi2'};
 labelDataUji = 'tabel32Ciri_Sesi2';
-for i = 1:3
-    for j = 1:9
+for i = 1:3 % Sesi1 Sesi2 Sesi1Sesi2
+    for j = 1:9 % Subjek 1 - 9
         dataLatih = eval([labelDataLatih{i} '.subjek' num2str(j)]);
         dataUji = eval([labelDataUji '.subjek' num2str(j)]);
 
-        disp(sprintf('Latih JST untuk Subjek%d',j));
+        disp(sprintf('Latih JST untuk Subjek%d - Sesi-%d',j,i));
 
-        % subjek{i} = JST2(dataLatih, 10, dataUji); 
         [JST{1}{i,j}, JST{2}{i,j}] = JST2(dataLatih, 10, dataUji);
 
         clear data;
